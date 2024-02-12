f474b70a66b1e198ed13da47baaa40038fa7f6e4
EZLLeno <EZLLeno@EZL-LEN>
2024-02-12 12:28:40 +0530

    Add project files.

---
+++ IBM12Feb2024CSharpMisc/ExceptionHandling.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBM12Feb2024CSharpMisc
+{
+    internal class ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+
+			try
+			{
+                int i = 0;
+
+                  try
+                {
+                   Console.Write(  "Enter Number :");
+                 i = int.Parse(Console.ReadLine());
+                Console.WriteLine(  i);
+                }
+
+                catch (FormatException e)
+                {
+
+                    Console.WriteLine(" Only Numeric ... " + e.Message);
+                    Console.ReadKey();
+
+                    throw new NullReferenceException("Null Ref Error ", e);
+
+                }
+                finally { Console.WriteLine("Finally Inner"); }
+
+
+                Console.WriteLine( $"Value of i={i}" );
+
+            }
+			
+        catch (OverflowException e)
+			{
+
+                Console.WriteLine( " number too Long ... " +  e.Message );
+                Console.ReadKey();
+            }
+        catch (Exception e)
+			{
+
+                Console.WriteLine( " Error Occured   " +  e.ToString() );
+                Console.ReadKey();
+            }
+            finally { Console.WriteLine("Finally Outter"); }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ IBM12Feb2024CSharpMisc/GCEg.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBM12Feb2024CSharpMisc
+{
+    internal class GCEg
+    {
+        static void Main()
+        {
+
+            using (Employee employee = new Employee())
+            {
+                 employee.EmpID = 1;
+          
+            }
+
+                   
+            Console.WriteLine(  "After GC");
+            Console.ReadKey();
+
+        }
+    }
+
+    class Employee:IDisposable
+    {
+        private bool disposedValue;
+
+        public int EmpID { get; set; }
+
+        ~Employee() {
+
+            Console.WriteLine(  "Fin Destructor Caller !!!");
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~Employee()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+}
